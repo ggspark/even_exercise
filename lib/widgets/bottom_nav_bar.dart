834289721bc46c61cfd199da6fca433ffb4ac31d
gaurav <gaurav@no-reply.com>
2022-11-22 19:57:24 +0530

    feat: added animation to bottom nav bar

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -33,87 +33,199 @@ class _BottomNavBarState extends State<BottomNavBar> {
             color: Color(darkBackgroundColor),
             borderRadius: BorderRadius.vertical(bottom: Radius.circular(24)),
           ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Stack(
             children: [
-              Expanded(
-                child: GestureDetector(
-                  onTap: () => setIndex(0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: getContainerColor(0),
-                      borderRadius: const BorderRadius.only(
-                        topRight: Radius.circular(40),
-                      ),
-                    ),
-                    child: Icon(
-                      Icons.home_outlined,
-                      color: getIconColor(0),
-                    ),
-                  ),
-                ),
+              BackgroundLayer(
+                selectedIndex: selectedIndex,
+                setIndex: setIndex,
               ),
-              Expanded(
-                child: GestureDetector(
-                  onTap: () => setIndex(1),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: getContainerColor(1),
-                      borderRadius: const BorderRadius.vertical(
-                        top: Radius.circular(40),
-                      ),
-                    ),
-                    child: Icon(
-                      Icons.history_outlined,
-                      color: getIconColor(1),
-                    ),
-                  ),
-                ),
+              AnimationLayer(
+                selectedIndex: selectedIndex,
               ),
-              Expanded(
-                child: GestureDetector(
-                  onTap: () => setIndex(2),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: getContainerColor(2),
-                      borderRadius: const BorderRadius.vertical(
-                        top: Radius.circular(40),
-                      ),
-                    ),
-                    child: Icon(
-                      Icons.spa_outlined,
-                      color: getIconColor(2),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: GestureDetector(
-                  onTap: () => setIndex(3),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: getContainerColor(3),
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                      ),
-                    ),
-                    child: Icon(
-                      Icons.person_outline,
-                      color: getIconColor(3),
-                    ),
-                  ),
-                ),
-              ),
+              IconLayer(selectedIndex: selectedIndex, setIndex: setIndex)
             ],
           ),
         )
       ]),
     );
   }
+}
+
+class BackgroundLayer extends StatelessWidget {
+  final int selectedIndex;
+  final Function setIndex;
+  const BackgroundLayer(
+      {super.key, required this.selectedIndex, required this.setIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          child: GestureDetector(
+            onTap: () => setIndex(0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: getContainerColor(0),
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(40),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: GestureDetector(
+            onTap: () => setIndex(1),
+            child: Container(
+              decoration: BoxDecoration(
+                color: getContainerColor(1),
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(40),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: GestureDetector(
+            onTap: () => setIndex(2),
+            child: Container(
+              decoration: BoxDecoration(
+                color: getContainerColor(2),
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(40),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: GestureDetector(
+            onTap: () => setIndex(3),
+            child: Container(
+              decoration: BoxDecoration(
+                color: getContainerColor(3),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 
   Color getContainerColor(int index) =>
       Color(selectedIndex == index ? bottomNavBgColor : darkBackgroundColor);
-  Color getIconColor(int index) => selectedIndex == index
+}
+
+class AnimationLayer extends StatelessWidget {
+  final int selectedIndex;
+  const AnimationLayer({super.key, required this.selectedIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          child: AnimatedSlide(
+            offset: Offset(selectedIndex * 1.0, 0),
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeIn,
+            child: Column(children: const [
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 16, 0, 4),
+                child: Icon(
+                  Icons.circle,
+                  color: Color(highlightColor),
+                ),
+              ),
+              Icon(
+                Icons.circle,
+                size: 8,
+                color: Color(darkBackgroundColor),
+              ),
+            ]),
+          ),
+        ),
+        Expanded(
+          child: Container(),
+        ),
+        Expanded(
+          child: Container(),
+        ),
+        Expanded(
+          child: Container(),
+        ),
+      ],
+    );
+  }
+}
+
+class IconLayer extends StatelessWidget {
+  final int selectedIndex;
+  final Function setIndex;
+  const IconLayer(
+      {super.key, required this.selectedIndex, required this.setIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          child: GestureDetector(
+            onTap: () => setIndex(0),
+            child: Container(
+              child: Icon(
+                Icons.home_outlined,
+                color: getIconColor(0, context),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: GestureDetector(
+            onTap: () => setIndex(1),
+            child: Container(
+              child: Icon(
+                Icons.history_outlined,
+                color: getIconColor(1, context),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: GestureDetector(
+            onTap: () => setIndex(2),
+            child: Container(
+              child: Icon(
+                Icons.spa_outlined,
+                color: getIconColor(2, context),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: GestureDetector(
+            onTap: () => setIndex(3),
+            child: Container(
+              child: Icon(
+                Icons.person_outline,
+                color: getIconColor(3, context),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Color getIconColor(int index, BuildContext context) => selectedIndex == index
       ? Theme.of(context).colorScheme.onPrimary
       : Theme.of(context).colorScheme.onSurface;
 }
