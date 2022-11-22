e33a14375fb154722d2b64ef386a7d9758cc88aa
gaurav <gaurav@no-reply.com>
2022-11-22 14:19:59 +0530

    feat: added timeline gradient

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -10,56 +10,76 @@ class HistoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: Text(
-              "My History",
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge
-                  ?.copyWith(fontWeight: FontWeight.w700),
+      child: Stack(
+        children: [
+          Container(
+            width: 4,
+            margin: const EdgeInsets.fromLTRB(50, 80, 20, 0),
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[
+                  Color(buttonColorLight),
+                  Color(darkBackgroundColor),
+                ],
+                tileMode: TileMode.clamp,
+              ),
             ),
           ),
-          Row(
-            children: [
-              Container(
-                width: 64,
-                height: 64,
-                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                        color: const Color(buttonColorLight), width: 6),
-                    shape: BoxShape.circle,
-                    color: const Color(buttonColor)),
-                child: Icon(
-                  Icons.add_rounded,
-                  size: 40,
-                  color: Theme.of(context).colorScheme.onPrimary,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Text(
+                  "My History",
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge
+                      ?.copyWith(fontWeight: FontWeight.w700),
                 ),
               ),
-              Text(
-                "Add consultation",
-                style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                    fontWeight: FontWeight.w700,
-                    color: const Color(buttonColor)),
+              Row(
+                children: [
+                  Container(
+                    width: 64,
+                    height: 64,
+                    margin:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: const Color(buttonColorLight), width: 6),
+                        shape: BoxShape.circle,
+                        color: const Color(buttonColor)),
+                    child: Icon(
+                      Icons.add_rounded,
+                      size: 40,
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                  ),
+                  Text(
+                    "Add consultation",
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        fontWeight: FontWeight.w700,
+                        color: const Color(buttonColor)),
+                  ),
+                ],
               ),
+              Expanded(
+                child: ListView(
+                  scrollDirection: Axis.vertical,
+                  children: const [
+                    ConsultationCard(),
+                    ConsultationCard(),
+                    ConsultationCard()
+                  ],
+                ),
+              )
             ],
           ),
-          Expanded(
-            child: ListView(
-              scrollDirection: Axis.vertical,
-              children: const [
-                ConsultationCard(),
-                ConsultationCard(),
-                ConsultationCard()
-              ],
-            ),
-          )
         ],
       ),
     );
